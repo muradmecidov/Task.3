e285b222bea981b353da6b7430b51708d8ca3545
muradmecidov <muradmecidov1499@gmail.com>
2023-04-05 22:51:58 +0400

    task3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 7, 9, 16, 21 };
+            InsertArray(ref num1, 38, 39);
+            for (int i = 0; i < num1.Length; i++)
+            {
+                Console.WriteLine(num1[i]);
+
+            }
+
+
+
+
+
+
+
+
+        }
+        public static void InsertArray( ref int[] arr ,  params int[] number )
+        {
+            int[] num2 = new int[arr.Length+ number.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                num2[i] = arr[i];
+            }
+
+            arr = num2;
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
